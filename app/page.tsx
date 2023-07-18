dc0da9cdce86256f357b2d0bb4405073dc940f50
Marichelle Bisuna <marichellebisuna@yahoo.com>
2023-07-18 15:03:11 +1000

    Navbar setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="flex-start flex-col paddings mb-16">
+     <h1>Categories</h1>
+     <h1>Posts</h1>
+     <h1>loadmore</h1>
+    </section>
   )
 }
 
---
+++ constants/index.ts
@@ -0,0 +1,23 @@
+export const NavLinks = [
+  { href: '/', key: 'Inspiration', text: 'Inspiration' },
+  { href: '/', key: 'Find Projects', text: 'Find Projects' },
+  { href: '/', key: 'Learn Development', text: 'Learn Development' },
+  { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+  { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots"
+]
